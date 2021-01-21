f54886ad4f293ef012479a789b7f93dd70b7eeb7
Thomas Nilefalk <thomas@nilefalk.se>
2021-01-21 22:58:08 +0100

    Change numbering scheme for venum itself (SemVer)

--- src/venum.version.c
+++ src/venum.version.c
@@ -1,19 +1,19 @@
-/* venum.c - Created by venum 4.2 beta */
+/* venum.version.c - Created by venum 4.2 beta */
 
 #include "venum.version.h"
 
 Product venum = {
   "venum",
   "version number maintainer",
-  "venum 4.2 beta",
-  "venum 4.2 beta -- version number maintainer (2020-09-24 23:07/linux)",
-  "2020-09-24",
-  "23:07:25",
-  "",
+  "venum 4.2beta1",
+  "venum 4.2beta1 -- version number maintainer (2021-01-21 22:53, Thomas@thoni64/cygwin64)",
+  "2021-01-21",
+  "22:53:30",
+  "Thomas",
   "thoni64",
-  "linux",
-  {"4.2 beta", 4, 2, 0, 1600981645, "beta"}
+  "cygwin64",
+  {"4.2beta1", 4, 2, 1, 1611266010, "beta"}
 };
 
 char *venumId =
-  "@(#)RELEASE venum 4.2 beta 2020-09-24 23:07:25@thoni64/linux, © 1988-1995 SoftLab ab";
+  "@(#)RELEASE venum 4.2beta1 2021-01-21 22:53:30 Thomas@thoni64/cygwin64, © 1988-1995 SoftLab ab";
