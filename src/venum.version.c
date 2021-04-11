c0ba8761f9752e35b5df255b5536b6debae3f778
Thomas Nilefalk <thomas@nilefalk.se>
2021-04-11 20:29:08 +0200

    Update venum version marking with numeric correction

--- src/venum.version.c
+++ src/venum.version.c
@@ -1,19 +1,19 @@
-/* venum.version.c - Created by venum 4.2 beta */
+/* venum.version.c - Created by venum 4.2 beta(i) */
 
 #include "venum.version.h"
 
 Product venum = {
   "venum",
   "version number maintainer",
-  "venum 4.2beta1",
-  "venum 4.2beta1 -- version number maintainer (2021-01-21 22:53, Thomas@thoni64/cygwin64)",
-  "2021-01-21",
-  "22:53:30",
-  "Thomas",
+  "venum 4.2 beta(1)",
+  "venum 4.2 beta(1) -- version number maintainer (2021-04-11 20:32/linux)",
+  "2021-04-11",
+  "20:32:33",
+  "",
   "thoni64",
-  "cygwin64",
-  {"4.2beta1", 4, 2, 1, 1611266010, "beta"}
+  "linux",
+  {"4.2 beta(1)", 4, 2, 1, 1618165953, "beta"}
 };
 
 char *venumId =
-  "@(#)RELEASE venum 4.2beta1 2021-01-21 22:53:30 Thomas@thoni64/cygwin64, © 1988-1995 SoftLab ab";
+  "@(#)RELEASE venum 4.2 beta(1) 2021-04-11 20:32:33@thoni64/linux, © 1988-1995 SoftLab ab";
