1e8af1512ef66fac127eb860eb1d38264535b16e
Thomas Nilefalk <thomas@nilefalk.se>
2021-04-11 21:21:51 +0200

    Remove space and parenthesis for venum's own version format

--- src/venum.version.c
+++ src/venum.version.c
@@ -1,19 +1,19 @@
-/* venum.version.c - Created by venum 4.2 beta(i) */
+/* venum.version.c - Created by venum 4.2 beta(1) */
 
 #include "venum.version.h"
 
 Product venum = {
   "venum",
   "version number maintainer",
-  "venum 4.2 beta(1)",
-  "venum 4.2 beta(1) -- version number maintainer (2021-04-11 20:32/linux)",
+  "venum 4.2beta1",
+  "venum 4.2beta1 -- version number maintainer (2021-04-11 21:21/linux)",
   "2021-04-11",
-  "20:32:33",
+  "21:21:15",
   "",
   "thoni64",
   "linux",
-  {"4.2 beta(1)", 4, 2, 1, 1618165953, "beta"}
+  {"4.2beta1", 4, 2, 1, 1618168875, "beta"}
 };
 
 char *venumId =
-  "@(#)RELEASE venum 4.2 beta(1) 2021-04-11 20:32:33@thoni64/linux, © 1988-1995 SoftLab ab";
+  "@(#)RELEASE venum 4.2beta1 2021-04-11 21:21:15@thoni64/linux, © 1988-1995 SoftLab ab";
